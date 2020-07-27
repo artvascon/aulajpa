04dc37df81c87993af1619ddd942faf07a934b96
Arthur <Arthur@arthur-pc>
2020-07-26 21:19:52 -0300

    Introdução a JPA - Curso Spring Boot com Ionic

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -9,37 +9,28 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		/*
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		*/
-		 
+
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		/*
-		em.getTransaction().begin();	
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		*/
-		
-		/*
-		Pessoa p = em.find(Pessoa.class, 2);
-		
-		System.out.println(p);
-		*/
-		
-		Pessoa p = em.find(Pessoa.class, 2);
-		
+
+
+//		Pessoa p = new Pessoa(2, null, null);
 		em.getTransaction().begin();
+		Pessoa p = em.find(Pessoa.class, 2);
+
+		System.out.println(p);
+		
 		em.remove(p);
 		em.getTransaction().commit();
-		
+
+		System.out.println("Pronto!");
 		em.close();
 		emf.close();
-		
+
+		/*
+		 * System.out.println(p1); System.out.println(p2); System.out.println(p3);
+		 */
+
 	}
 
 }
--- src/dominio/Pessoa.java
+++ src/dominio/Pessoa.java
@@ -6,29 +6,25 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	private String email;
-
-	public Pessoa() {
-	}
+	
+	public Pessoa() {}
 
 	public Pessoa(Integer id, String nome, String email) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + "]";
 	}
 
 	public Integer getId() {
@@ -54,5 +50,12 @@ public class Pessoa implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+	
+	
 
 }
